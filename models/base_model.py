dfe7b2d9c143a9784158927511cab1d225d7b55f
thala254 <thalamine@gmail.com>
2022-08-02 14:01:17 +0300

    completed task 4

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,20 @@ from datetime import datetime
 
 class BaseModel:
     """ class to be used as the base class for other nodels in th project"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initializes all instances of the BaseModel class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key in kwargs.keys():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(kwargs[key]))
+                elif key == "__class__":
+                    pass
+                else:
+                    setattr(self, key, kwargs[key])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation of the BaseModel class"""
