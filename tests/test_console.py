4ba7897af8a7bda81e621e947326e1ef0097f666
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-08-04 22:19:04 +0300

    My commit

--- tests/test_console.py
+++ tests/test_console.py
@@ -128,7 +128,7 @@ class TestConsole(unittest.TestCase):
                              fake_output.getvalue())
         with patch('sys.stdout', new=StringIO()) as fake_output:
             self.typing.onecmd("update You")
-            self.assertEqual("** class doesnt exist **\n",
+            self.assertEqual("** class doesn't exist **\n",
                              fake_output.getvalue())
         with patch('sys.stdout', new=StringIO()) as fake_output:
             self.typing.onecmd("update User")
