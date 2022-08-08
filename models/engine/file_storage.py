27318cb885638c764d14c38cc55d7afe834e11a1
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-08-08 13:24:38 +0300

    My commit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,7 +30,8 @@ class FileStorage:
 
     def save(self):
         """serializes __objects to the JSON file path"""
-       json_obj = {key: val.to_dict() for key, val in FileStorage.__objects.items()}
+        json_obj = {key: val.to_dict() 
+                    for key, val in FileStorage.__objects.items()}
         with open(FileStorage.__file_path, "w") as fd:
             json.dump(json_obj, fd)
 
